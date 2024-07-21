9f2becfa00335c55e3af91aeed6bed8079a72ba9
Andrew Youngtaek Song <y_song179344@fanshaweonline.ca>
2024-07-21 16:40:51 -0400

    - Added Navigation

--- App.js
+++ App.js
@@ -1,12 +1,63 @@
+import { FontAwesome } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer, } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View
+} from 'react-native';
+
+import Transaction from './componenets/Transaction';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View>
+        <StatusBar style="auto" />
+
+        <Tab.Navigator>
+          {/* home */}
+          <Tab.Screen name='Transation' options={{
+            headerShown: true,
+            headerTintColor: '#fff',
+            headerStyle: {
+              backgroundColor: '#008080'
+            },
+            title: 'Transaction',
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name='file' size={size} color={color} />
+            )
+          }}>
+            {(props) => (
+              <Transaction />
+            )}
+          </Tab.Screen>
+
+          {/* Search */}
+          <Tab.Screen name='Summary' options={{
+            headerShown: true,
+            title: 'Summary',
+            headerTintColor: '#fff',
+            headerStyle: {
+              backgroundColor: '#008080'
+            },
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome name='info' size={size} color={color} />
+            )
+          }}>
+            {(props) => (
+              <Search {...props}
+                movies={movies}
+                onAddTask={handleAddTask}
+                onSearchMovie={onSearchMovie}
+                onAddMovie={onAddMovie} />
+            )}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </View>
+    </NavigationContainer >
   );
 }
 
---
+++ componenets/Transaction.js
@@ -0,0 +1,14 @@
+import {
+    Text,
+    View
+} from 'react-native'
+
+export default function Transaction(props) {
+    return (
+        <>
+            <View>
+                <Text>Transaction</Text>
+            </View>
+        </>
+    )
+}
